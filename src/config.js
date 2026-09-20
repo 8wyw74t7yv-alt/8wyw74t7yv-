30c1079a86a773730c11a3b0d15507c2259e312b
8wyw74t7yv-alt <8wyw74t7yv@privaterelay.appleid.com>
2026-09-21 00:22:45 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 
 module.exports = {
   botToken: process.env.BOT_TOKEN,
+  geminiApiKey: process.env.GEMINI_API_KEY, // <-- BU QATORNI QO'SHIB QO'YAMIZ
   adminId: parseInt(process.env.ADMIN_ID || "8913846037", 10),
   channelUsername: process.env.CHANNEL_USERNAME || "@muzxs",
   channelLink: process.env.CHANNEL_LINK || "https://t.me/muzxs",
   githubCoverUrl: process.env.GITHUB_COVER_URL || "",
   auddApiKey: process.env.AUDD_API_KEY || "",
-    // Ruxsat berilgan reaksiyalar (ko'plik qilib yubormaslik uchun 1-2 taga qisqartiramiz)
+  
+  // Ruxsat berilgan reaksiyalar
   reactions: ["🔥", "👍"],
-
   
   // Shablondagi artist va standart nom
   defaultArtist: "-MuzXs",

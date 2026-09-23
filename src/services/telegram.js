d411d17c2e76473db8adddba23bbd4e1c34c751a
8wyw74t7yv-alt <8wyw74t7yv@privaterelay.appleid.com>
2026-09-23 17:08:12 +0500

    Delete src/services/telegram.js

--- src/services/telegram.js
+++
@@ -1,7 +0,0 @@
-// Telegram xabariga avtomatik reaksiya bosish funksiyasi (Hozirda nofaol)
-async function setAutoReactions(telegram, chatId, messageId) {
-  // Reaksiyalar olib tashlangani uchun funksiya bo'sh qoldirildi
-  return true;
-}
-
-module.exports = { setAutoReactions };
